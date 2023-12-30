c7e7c53cb690e588231db20639dee2e802a5977a
Owen Wu <yubing744@163.com>
2023-12-30 20:17:10 +0800

    feat: gen svg

---
+++ count.py
@@ -0,0 +1,21 @@
+import json
+
+# Function to validate the 'character' field in each entry
+def validate_json(json_file_path):
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    entries = []
+    for entry in data:
+        entries.append(entry)
+            
+    return entries
+
+# Path to the JSON file
+json_file_path = 'shuowen_jiezi.json'
+
+# Call the validate function and store the invalid entries
+entries = validate_json(json_file_path)
+
+# Output the invalid entries
+print(len(entries))
---
+++ gensvg.py
@@ -0,0 +1,30 @@
+import json
+import os
+import xml.etree.ElementTree as ET
+
+# Load the JSON data
+with open('shuowen_jiezi_test.json', 'r') as file:
+    data = json.load(file)
+
+# Ensure the 'dist' directory exists
+os.makedirs('dist', exist_ok=True)
+
+# Generate SVG images and save them to the 'dist' directory
+for item in data:
+    # Load the SVG template
+    with open('template.svg', 'r') as file:
+        svg_template = file.read()
+
+    # Replace placeholders in the SVG template with actual data
+    svg_modified = svg_template.replace('{{char}}', item['character'])
+    svg_modified = svg_modified.replace('{{utf8Hex}}', str(item['id']))
+
+    # Parse the modified SVG
+    tree = ET.ElementTree(ET.fromstring(svg_modified))
+
+    # Save the modified SVG
+    output_path = os.path.join('dist', f"{item['id']}.svg")
+    tree.write(output_path)
+
+# Variables to output
+svg_files_generated = [os.path.join('dist', f"{item['id']}.svg") for item in data]
